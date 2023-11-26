5604c2ae7df315564a323c4e7f6843412754b9a1
syafiqo.ararashy <syafiqo.arashy@ui.ac.id>
2023-11-26 13:47:16 +0700

    feat: created landing component

--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,33 @@
-import Image from 'next/image'
-import styles from './page.module.css'
+'use client';
+import styles from './page.module.scss'
+import { useEffect, useState } from 'react'
+import Landing from '../components/Landing';
+import { AnimatePresence } from 'framer-motion';
 
 export default function Home() {
-  return (
-    <main className={styles.main}>
 
-    </main>
-  )
+    useEffect( () => {
+
+        (
+
+            async () => {
+
+                const LocomotiveScroll = (await import('locomotive-scroll')).default
+
+                const locomotiveScroll = new LocomotiveScroll();
+
+            }
+
+        )()
+
+    }, [])
+
+    return (
+        <main className={styles.main}>
+            {/*<AnimatePresence mode='wait'>*/}
+            {/*    {isLoading && <Preloader />}*/}
+            {/*</AnimatePresence>*/}
+            <Landing />
+        </main>
+    )
 }
---
+++ src/components/landing/index.jsx
@@ -0,0 +1,59 @@
+'use client';
+import Image from 'next/image';
+import styles from './style.module.css';
+import gsap from 'gsap';
+import {useEffect, useRef} from "react";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+
+export default function Home() {
+
+    const firstText = useRef(null);
+    const secondText = useRef(null);
+    const slider = useRef(null);
+    let xPercent = 0;
+    let direction = -1;
+
+    useEffect(() => {
+        gsap.registerPlugin(ScrollTrigger)
+        requestAnimationFrame(animation);
+
+        gsap.to(slider.current, {
+            scrollTrigger: {
+                trigger: document.documentElement,
+                start: 0,
+                end: window.innerHeight,
+                scrub: 0.25,
+                onUpdate: e => direction = e.direction * -1
+            },
+            x: "-=300px",
+        })
+    }, []);
+
+    const animation = () => {
+        if (xPercent <= -100) {
+            xPercent = 0;
+        }
+        if (xPercent > 0) {
+            xPercent = -100;
+        }
+        gsap.set(firstText.current, {xPercent: xPercent});
+        gsap.set(secondText.current, {xPercent: xPercent});
+        xPercent += 0.1 * direction;
+        requestAnimationFrame(animation);
+    }
+
+    return (
+        <main className={styles.landing}>
+            <Image
+                fill={true}
+                src={"/images/background.jpg"}
+                alt={"image"}/>
+            <div className={styles.sliderContainer}>
+                <div ref={slider} className={styles.slider}>
+                    <p ref={firstText}>Developer and Designer -</p>
+                    <p ref={secondText}>Developer and Designer -</p>
+                </div>
+            </div>
+        </main>
+    )
+}
