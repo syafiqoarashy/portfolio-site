b9b9f461b788c6f488548efc3cf28f98c07551c5
syafiqo.ararashy <syafiqo.arashy@ui.ac.id>
2024-07-10 01:54:52 +1000

    fix scrolling issue

--- src/app/page.js
+++ src/app/page.js
@@ -1,37 +1,37 @@
 'use client';
-import styles from './page.module.scss'
 import { useEffect, useState } from 'react'
+import styles from './page.module.scss'
 import Landing from '../components/landing';
 import Description from '../components/description';
 import Projects from '../components/projects';
 import Footer from '../components/footer';
-import { Metadata } from 'next';
-
 
 export default function Home() {
+    const [locomotiveScroll, setLocomotiveScroll] = useState(null);
 
-    useEffect( () => {
+    useEffect(() => {
+        if (typeof window !== 'undefined') {
+            (async () => {
+                const LocomotiveScroll = (await import('locomotive-scroll')).default;
+                const scroll = new LocomotiveScroll({
+                    el: document.querySelector('[data-scroll-container]'),
+                    smooth: true,
+                    smoothMobile: false,
+                    resetNativeScroll: true
+                });
+                setLocomotiveScroll(scroll);
+            })();
+        }
 
-        (
-
-            async () => {
-
-                const LocomotiveScroll = (await import('locomotive-scroll')).default
-
-                const locomotiveScroll = new LocomotiveScroll();
-
-
+        return () => {
+            if (locomotiveScroll) {
+                locomotiveScroll.destroy();
             }
-
-        )()
-
-    }, [])
+        }
+    }, []);
 
     return (
-        <main className={styles.main}>
-            {/*<AnimatePresence mode='wait'>*/}
-            {/*    {isLoading && <Preloader />}*/}
-            {/*</AnimatePresence>*/}
+        <main className={styles.main} data-scroll-container>
             <Landing />
             <Description />
             <Projects />
